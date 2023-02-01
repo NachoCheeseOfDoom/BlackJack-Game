60538ee2a679995b7ed0a651646d654d4daaff4e
NachoCheeseOfDoom <85528067+NachoCheeseOfDoom@users.noreply.github.com>
2023-02-01 13:35:50 -0600

    Update

--- main.js
+++ main.js
@@ -1,12 +1,38 @@
-const firstCard = -20;
-const secondCard = 19;
-let sum = firstCard + secondCard;
-console.log(sum)
+const messageEl = document.getElementById('message-el');
+const sumEl = document.getElementById('sum-el');
+const cardsEl = document.getElementById('cards-el');
 
-if (sum < 21) {
-  console.log("Do you want to draw a new card?")
-} else if (sum === 21) {
-  console.log('congrats 🥳');
-} else {
-  console.log('😢')
+const firstCard = 2;
+const secondCard = 15;
+let sum = firstCard + secondCard;
+let cards = [firstCard, secondCard];
+
+let hasBlackJack = false;
+let isAlive = true;
+let message = '';
+
+function startGame() {
+  renderGame();
+}
+
+function renderGame() {
+  sumEl.innerHTML = "Sum: " + sum;
+  cardsEl.innerHTML = `Cards: ${cards[0]} ${cards[1]}`
+  if (sum <= 20) {
+    message = "Do you want to draw a new card?";
+  } else if (sum === 21) {
+    message = "You've got Blackjack!";
+    hasBlackJack = true;
+  } else {
+    message = "You're out of the game!";
+    isAlive = false;
+  }
+  messageEl.innerHTML = message
+}
+
+
+function newCard() {
+  let newCard = 4;
+  sum = sum + newCard;
+  renderGame();
 }
